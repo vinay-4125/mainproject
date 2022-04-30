16d580939d298e2bdb109d1fa38b7bb112e207ab
vinay-4124 <vinaychaudhari4125@gmail.com>
2022-04-30 13:59:41 +0530

    adding tabledata

--- components/user/UserOrderList.js
+++ components/user/UserOrderList.js
@@ -36,7 +36,7 @@ const UserOrderList = () => {
     });
     // console.log(ordersArray)
     setPreviousOrder(ordersArray);
-    console.log(ordersArray);
+    // console.log(ordersArray);
   };
   useEffect(() => {
     getDataPreviousOrder();
@@ -47,154 +47,176 @@ const UserOrderList = () => {
   return (
     <>
       <div className="flex flex-col mt-8">
-  {/* <div className="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8"> */}
-  <div className="py-2 -my-2 overflow-x-auto">
-    <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
-      {previousOrder.length === 0 ? (
-        <>
-          <div className="mt-48 sm:mt-32">
-            <div className="flex justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-16 w-16"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <p className="text-2xl my-auto font-bold">
-                Oops! No previous order
-              </p>
-            </div>
-            <div className="mt-6 justify-center text-center text-sm text-gray-500">
-              <p>
-                <span aria-hidden="true">&larr;</span>
-                <button
-                  type="button"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                  onClick={() => router.push("/")}
-                >
-                  Continue Shopping
-                </button>
-              </p>
-            </div>
-          </div>
-        </>
-      ) : (
-        <table className="min-w-full">
-          {/* <table className=""> */}
-          <thead>
-            <tr className="self-center">
-              <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                Customer Name
-              </th>
-              <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                Email
-              </th>
-              <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                Cart Quantity
-              </th>
-              <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                Price
-              </th>
-              {/* <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+        {/* <div className="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8"> */}
+        <div className="py-2 -my-2 overflow-x-auto">
+          <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
+            {previousOrder.length === 0 ? (
+              <>
+                <div className="mt-48 sm:mt-32">
+                  <div className="flex justify-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-16 w-16"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    <p className="text-2xl my-auto font-bold">
+                      Oops! No previous order
+                    </p>
+                  </div>
+                  <div className="mt-6 justify-center text-center text-sm text-gray-500">
+                    <p>
+                      <span aria-hidden="true">&larr;</span>
+                      <button
+                        type="button"
+                        className="font-medium text-indigo-600 hover:text-indigo-500"
+                        onClick={() => router.push("/")}
+                      >
+                        Continue Shopping
+                      </button>
+                    </p>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <table className="min-w-full">
+                {/* <table className=""> */}
+                <thead>
+                  <tr className="self-center">
+                    <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                      Customer Name
+                    </th>
+                    <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                      Product Name
+                    </th>
+                    <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                      Prouduct Image
+                    </th>
+                    <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                      Email
+                    </th>
+                    <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                      Cart Quantity
+                    </th>
+                    <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                      Price
+                    </th>
+                    {/* <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
                       Cart Items
                     </th> */}
-            </tr>
-          </thead>
-          <tbody className="bg-white">
-            {previousOrder.map((item, id) => (
-              <tr key={id}>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <div className="flex items-center">
-                    <div className="ml-4">
-                      <div className="text-sm font-medium leading-5 text-center text-gray-900">
-                        {item.orderObj.user.displayName}
-                      </div>
-                    </div>
-                  </div>
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <div className="text-sm leading-5 text-center text-gray-500">
-                    {item.orderObj.user.email}
-                  </div>
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <div className="text-sm leading-5 text-center text-gray-500">
-                    {/* {item.orderObj.user.email} */}
-                    {item.orderObj.cart.cartTotalQuantity}
-                  </div>
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <div className="text-sm leading-5 text-center text-gray-500">
-                    {/* {item.orderObj.user.email} */}
-                    {item.orderObj.cart.cartTotalAmount + 70}
-                  </div>
-                </td>
-                {/* <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                  </tr>
+                </thead>
+                <tbody className="bg-white">
+                  {previousOrder.map((item, id) => (
+                    <tr key={id}>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div className="flex items-center">
+                          <div className="ml-4">
+                            <div className="text-sm font-medium leading-5 text-center text-gray-900">
+                              {item.orderObj.user.displayName}
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div className="text-sm leading-5 text-center text-gray-500">
+                          {item.orderObj.cart.cartItems[0].name}
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div className="text-sm leading-5 text-center text-gray-500">
+                          {/* {item.orderObj.user.email} */}
+                          <Image
+                          src={item.orderObj.cart.cartItems[0].url}
+                          height="80"
+                          width="80"
+                          />
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div className="text-sm leading-5 text-center text-gray-500">
+                          {item.orderObj.user.email}
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div className="text-sm leading-5 text-center text-gray-500">
+                          {/* {item.orderObj.user.email} */}
+                          {item.orderObj.cart.cartTotalQuantity}
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div className="text-sm leading-5 text-center text-gray-500">
+                          {/* {item.orderObj.user.email} */}
+                          {item.orderObj.cart.cartTotalAmount + 70}
+                        </div>
+                      </td>
+                      {/* <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                         <div className="text-sm leading-5 text-center text-gray-500">
                           <CartOrderInfo item={item} />
                         </div>
                       </td> */}
-              </tr>
-            ))}
-          </tbody>
-          {/* <TableContainer className="h-44">
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Image</TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell align="right">Quantity</TableCell>
-                  <TableCell align="right">Price</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {previousOrder.map((data, id) => {
-                  
-                  <TableRow key={id}>
-                    <TableCell>
-                      <Image
-                        src={data.orderObj.cart.cartItems[0].url}
-                        alt={data.orderObj.cart.cartItems[0].name}
-                        width="50"
-                        height="50"
-                      ></Image>
-                    </TableCell>
+                    </tr>
+                  ))}
+                </tbody>
+                {/* <TableContainer className="h-44">
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Image</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {previousOrder.map((data, id) => {
+                        {
+                          console.log(data.orderObj.cart.cartItems);
+                        }
+                        <TableRow key={id}>
+                          <TableCell>
+                            <Image
+                              src={data.orderObj.cart.cartItems[0].url}
+                              alt={data.orderObj.cart.cartItems[0].name}
+                              width="50"
+                              height="50"
+                            ></Image>
+                          </TableCell>
 
-                    <TableCell>
-                      <Typography>
-                        {data.orderObj.cart.cartItems[0].name}
-                      </Typography>
-                    </TableCell>
-                    <TableCell align="right">
-                      <Typography>
-                        {data.orderObj.cart.cartItems[0].cartQuantity}
-                      </Typography>
-                    </TableCell>
-                    <TableCell align="right">
-                      <Typography>
-                        {data.orderObj.cart.cartItems[0].price}
-                      </Typography>
-                    </TableCell>
-                  </TableRow>;
-                  
-                })}
-              </TableBody>
-            </Table>
-          </TableContainer> */}
-        </table>
-      )}
-    </div>
-  </div>
-</div>;
-
+                          <TableCell>
+                            <Typography>
+                              {data.orderObj.cart.cartItems[0].price}
+                            </Typography>
+                          </TableCell>
+                          <TableCell align="right">
+                            <Typography>
+                              {data.orderObj.cart.cartItems[0].cartQuantity}
+                            </Typography>
+                          </TableCell>
+                          <TableCell align="right">
+                            <Typography>
+                              {data.orderObj.cart.cartItems[0].price}
+                            </Typography>
+                          </TableCell>
+                        </TableRow>;
+                      })}
+                    </TableBody>
+                  </Table>
+                </TableContainer> */}
+              </table>
+            )}
+          </div>
+        </div>
+      </div>
+      ;
     </>
   );
 };
